387e3fe5d9303c2a17375008cb86e041d7e9d95c
max <143447775+dj-maxito@users.noreply.github.com>
2025-12-05 23:28:40 -0300

    Rutas arregladas + conexión a EventDetail
    Falta que se muestren las imágenes cuando se muestran eventos específicos, se arregló el error de los archivos mal colocados en src/api

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import RootStack from './navigation/RootStack';
+import RootStack from './src/navigation/RootStack';
 
 export default function App() {
   return (
---
+++ src/navigation/RootTabs.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+/* Screens */
+import HomeScreen from '../screens/HomeScreen';
+import Purchases from '../screens/Purchases';
+
+const Tab = createBottomTabNavigator();
+
+export default function RootTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: keyof typeof Ionicons.glyphMap = 'home';
+
+          if (route.name === 'HomeTab') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Purchases') {
+            iconName = focused ? 'cart' : 'cart-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#2E27F5',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen name="HomeTab" component={HomeScreen} options={{ title: 'Home' }} />
+      <Tab.Screen name="Purchases" component={Purchases} options={{ title: 'Purchases' }} />
+    </Tab.Navigator>
+  );
+}
