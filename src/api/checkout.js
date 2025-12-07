e97303279b339b3397031a36f21dcfdfdc4e2297
benjasmines <143446989+Benjasmines@users.noreply.github.com>
2025-12-07 15:26:01 -0300

    Delete src/api directory

--- src/api/checkout.js
+++
@@ -1,9 +0,0 @@
-import { ENDPOINTS } from "../config/api";
-import { apiRequest } from "./client";
-
-export function confirmCheckout(reservationId) {
-  return apiRequest(ENDPOINTS.CHECKOUT, {
-    method: "POST",
-    body: JSON.stringify({ reservationId })
-  });
-}
--- src/api/tickets.ts
+++
@@ -1,6 +0,0 @@
-import api from "./client";
-
-export async function getTickets() {
-  const res = await api.get("/tickets");
-  return res.data;
-}
